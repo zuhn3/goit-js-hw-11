a9453b7ae7559eb197c9ed6b3dc66d9ea5e22033
zuhn3 <aop1603@gmail.com>
2025-04-18 20:00:34 +0100

    changes in package and vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/goit-js-hw-11/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
